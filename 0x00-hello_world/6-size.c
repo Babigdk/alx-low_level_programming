4c92b59e2206bfeeb3ef86270747043fdcc4f29a
Babigdk <Babigdk@gmail.com>
2022-06-04 09:02:12 -0700

    size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,8 +15,8 @@ int main(void)
 
 	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-	printf("siiize of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
-	printf("size of a long long: %lu byte(s)\n", (unsigned long)sizeof(lli));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
 	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
