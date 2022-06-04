29140efda47d6068d418422986a03d7222c50a59
Babigdk <Babigdk@gmail.com>
2022-06-04 01:28:46 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
+#!/bin/bash
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-	puts("\Programming is building a multilingual puzzle\n");
+	puts("Programming is building a multilingual puzzle\n");
 	return (0);
 }	
