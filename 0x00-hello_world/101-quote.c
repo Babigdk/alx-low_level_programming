e1b8ad0d04d176cfc726038d662d2c906c48ed2f
Babigdk <Babigdk@gmail.com>
2022-06-04 15:04:56 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -Etry pont
+ *
+ * Return: Always 1 (success)
+ */
+int main(void)
+{
+	write(STDOUT, "and that piece of art is usefull
+			\" - Dora korpar, 2015-10-19\n", 59);
+	return(1);
+}
