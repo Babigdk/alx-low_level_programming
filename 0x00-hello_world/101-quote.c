8356122bc827e35cd4884571ef8a7f53a006d667
Babigdk <Babigdk@gmail.com>
2022-06-05 04:11:23 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,11 +7,6 @@
  */
 int main(void)
 {
-<<<<<<< HEAD
 	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 61);
-=======
-	write(2, "and that piece of art is useful
-\" - Dora korpar, 2015-10-19\n", 59);
->>>>>>> 1113917ac05bb51ff8285fd3c45470ffae034ebc
 	return(1);
 }
