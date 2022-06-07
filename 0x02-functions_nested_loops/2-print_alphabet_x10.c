341df75fbefd5e1b68e5769894e494cdeb840a03
Babigdk <Babigdk@gmail.com>
2022-06-07 14:56:22 -0700

    Alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_alphabet_x10 - print 10 times alphabet in lower case
+ */
+void print_alphabet_x10(void)
+{
+	int n = 0;
+	char lett;
+
+	while(n + 1 <= 9)
+	{
+		for (lett = 'a'; lett <= 'z'; lett++)
+		{
+			_putchar(lett);
+		}
+		_putchar('\n');
+	}
+}
