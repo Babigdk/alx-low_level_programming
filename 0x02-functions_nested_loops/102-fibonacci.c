9cb9b1f3a44320c56c7b4c18dd4af26b63b48bad
Babigdk <Babigdk@gmail.com>
2022-06-07 18:54:43 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - return the fibonnaci
+ *
+ * Return: Always 0 
+ */
+int main(void)
+{
+	int counter;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+
+	for ( counter = 0; counter < 50; counter++)
+	{
+		sum = fib1 + fib2;
+		printf("%lu", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (counter == 49)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf(", ");
+		}
+	}
+}
