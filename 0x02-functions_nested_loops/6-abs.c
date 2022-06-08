f1d612e8a66baec26b8abab073801ae55011451a
Babigdk <Babigdk@gmail.com>
2022-06-07 17:39:16 -0700

    return absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - return the absolute vale
+ * @n: the number to return
+ *
+ * Return: the absolute
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
