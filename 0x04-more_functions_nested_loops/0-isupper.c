8115d1aa8f089b621000b17af697d142d2f7ba59
Babigdk <Babigdk@gmail.com>
2022-06-09 13:10:26 -0700

    more

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -10,16 +10,9 @@
 
 int _isupper(int c)
 {
-	char uppercase = 'A';
-	int isupper = 0;
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 
-	for (; uppercase <= 'Z'; uppercase++)
-	{
-		if (c == uppercase)
-		{
-			isupper = 1;
-			break;
-		}
-	}
-	return (isupper);
 }
