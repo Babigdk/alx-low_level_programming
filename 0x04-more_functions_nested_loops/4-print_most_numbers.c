47faddbacdb86ef4fdfb9066e5d51d5ccc1a8cf3
Babigdk <Babigdk@gmail.com>
2022-06-10 09:52:36 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - entry point
+ *
+ * Description: numbers with _putchar
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		if (num = '2' || num = '4')
+			_putchar(i);
+		_putchar('\n');
+	}
+}
