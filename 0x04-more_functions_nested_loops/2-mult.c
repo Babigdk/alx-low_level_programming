831cef1a749e36beffb796609e1f8a99e1979e37
Babigdk <Babigdk@gmail.com>
2022-06-10 09:36:51 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/2-mult.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mult - input point
+ * Description: mult 2 number
+ * @a: first number
+ * @b: second number
+ * Return: answer
+ */
+int mult(int a, int b)
+{
+	return (a * b);
+}
