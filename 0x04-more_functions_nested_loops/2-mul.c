34d878cf3ec1cf528dc7b4d8f3e52471decec1d7
Babigdk <Babigdk@gmail.com>
2022-06-10 09:39:13 -0700

    hello

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * mult - input point
+ * mul - input point
  * Description: mult 2 number
  * @a: first number
  * @b: second number
  * Return: answer
  */
-int mult(int a, int b)
+int mul(int a, int b)
 {
 	return (a * b);
 }
