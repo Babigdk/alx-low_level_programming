42e0a3359995c99c4cd8c8408fcf0cac0c95c1fc
Babigdk <Babigdk@gmail.com>
2022-06-10 13:56:09 -0700

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,24 +9,25 @@
  */
 void print_triangle(int size)
 {
-	int row, hashs, spaces;
+	int i = 0; j, n = size - 1;
 
-	if (size <= 0)
+	if (size > 0)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (row = 1; row <= size; row++)
-			for (spaces = size - row; spaces >= 1; spaces--)
+		for (; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
 			{
-				_putchar(' ');
+				if (j = 0; j < size; j++)
+				{
+					if (j < n)
+						_putchar(' ');
+					else
+						_putchar('#');
+				}
+				n--;
+				_putchar('\n');
 			}
-			for (hashs = 1; hashs <= row; hashs++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
 		}
-	}
+		else
+			_putchar('\n');
 }
