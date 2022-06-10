b3a46a1fbd82841ca3908047a4a84c7163698ce6
Babigdk <Babigdk@gmail.com>
2022-06-10 14:54:33 -0700

    prime number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,30 +8,41 @@ void print_integer(int m);
  */
 void print_number(int n)
 {
-	if (n == 0)
-		_putchar('0');
-	else if (n < 0)
+	int i, j, digit, digits, power;
+	unsigned int temp, numchar, number;
+
+	digit = 0;
+	if (n < 0)
 	{
 		_putchar('_');
-		print_integer(n * -1);
+		temp = -n;
 	}
 	else
-		print_integer(n);
-}
+	{
+		temp = n;
+	}
 
-/**
- * print_integer - a function to be call
- * @m: input unsigned
- * Return: Always 0
- */
+	number = temp;
 
-void print_integer(int m)
-{
-	int i = 1000000000;
+	while (number >= 10)
+	{
+		number = number / 10;
+		digit++;
+	}
+	digits = digit + 1;
+	power = 1;
+	i = 1;
 
-	for (; i >= 1; i /= 10)
-		if (m / i != 0)
-		{
-			_putchar((m / i) % 10 + '0');
-		}
+	while (i < digits)
+	{
+		power = power * 10;
+		i++;
+	}
+	j = power;
+	while (j >= 1)
+	{
+		number = (temp / j) % 10;
+		_putchar(number + '0');
+		j = j / 10;
+	}
 }
