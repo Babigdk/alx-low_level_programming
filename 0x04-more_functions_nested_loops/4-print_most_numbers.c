f6855c602f39af0bab913627add4621449effdb2
Babigdk <Babigdk@gmail.com>
2022-06-10 09:55:28 -0700

    isdigit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,12 +10,12 @@
 
 void print_most_numbers(void)
 {
-	int num;
+	int num = '0';
 
-	for (num = '0'; num <= '9'; num++)
+	for (; num <= '9'; num++)
 	{
-		if (num = '2' || num = '4')
+		if (num != '2' && num != '4')
 			_putchar(i);
-		_putchar('\n');
 	}
+	__putchar('\n');
 }
