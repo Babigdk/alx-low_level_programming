1f5379df7babf88a712385cfe2dc05a71b0e2e6e
Babigdk <Babigdk@gmail.com>
2022-06-13 16:54:30 -0700

    98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,10 @@
 #include "main.h"
 
-/*
- * reset_to_98 - function with one int argument
- * @n: arg of type int
+/**
+ * reset_to_98 - function that take one arg
+ * @n: input
  *
- * Description: Using pointer as parameter to update value it points to 98
- *
- * Return: void 
+ * Return: vaoid
  */
 void reset_to_98(int *n)
 {
