4b5a40d5f3ace9699ec5b52074bde39598235d06
Babigdk <Babigdk@gmail.com>
2022-06-13 12:55:03 -0700

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /*
- * reset_to_98 - function to reset to 98
- * @*n: intput
+ * reset_to_98 - set a pointer function to reset to 98
+ * @n: intput
  * Return: void
  */
 void reset_to_98(int *n)
