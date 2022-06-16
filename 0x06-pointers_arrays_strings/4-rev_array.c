abfd9c457bfc5a4d3afdc0dc3ba971abb4ebbcde
Babigdk <Babigdk@gmail.com>
2022-06-16 05:52:50 -0700

    reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,20 +9,12 @@
  */
 void reserve_array(int *a, int n)
 {
-	int *p, i, aux, k;
+	int i, c;
 
-	*p = a;
-
-	for (i = 1; i < n; i++)
-	{
-		p++;
-	}
-
-	for (k = 0; k < i / 2; k++)
-	{
-		aux = a[k];
-		a[k] = *p;
-		*p = aux;
-		p--;
-	}
+	for (i = 0; (i < (n - 1) / 2; i++)
+		{
+		c = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = c;
+		}
 }
