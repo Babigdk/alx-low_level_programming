8808a8ad2352449691d02f08f95652fcb23cf55e
Babigdk <Babigdk@gmail.com>
2022-06-16 04:13:55 -0700

    cat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,17 +13,15 @@ char *_strcat(char * dest, char *scr)
 	int b;
 
 	a = 0;
+	b = 0;
 
-	while (dest[a] != '\0')
+	while (*(dest + a))
 	{
 		a++;
 	}
 
-	b = 0;
-
-	while (src[b] != '\0')
+	while (*(dest + a) = *(src + b))
 	{
-		dest[a] = src[b];
 		a++;
 		b++;
 	}
