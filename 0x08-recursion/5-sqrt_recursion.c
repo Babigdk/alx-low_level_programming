7442fcd2d30b0b654f77677568ca53b481bcc9a8
Babigdk <Babigdk@gmail.com>
2022-06-22 15:14:40 -0700

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,9 +1,5 @@
 #include "main.h"
-/**
- * _sqrt_recursion - function to return natural root of number
- * @n: square of n
- * Return: int
- */
+
 /**
  * sqrt1 - evaluate from 1 to n
  * @a: n input of main function
@@ -18,6 +14,11 @@ int sqrt1(int a, int b)
 		return (-1);
 	return (sqrt1(a, b + 1));
 }
+/**
+ * _sqrt_recursion - returns natural square root
+ * @n: input number
+ * Return: 1 on success
+ */
 int _sqrt_recursion(int n)
 {
 	return (sqrt1(n, 1));
