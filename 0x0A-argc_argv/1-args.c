a2123a0a8543f9e71abe804d3ebff091dbb78261
Babigdk <Babigdk@gmail.com>
2022-06-24 13:48:32 -0700

    argc

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * #argv: second para
  * Return: 0
  */
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__ ((unused)))
 {
 	int i;
 	while (i < argc)
