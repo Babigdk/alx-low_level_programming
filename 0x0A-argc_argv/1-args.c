b6022e7264a15ef0c47850ec059e3c890019925b
Babigdk <Babigdk@gmail.com>
2022-06-24 13:46:45 -0700

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: first
+ * #argv: second para
+ * Return: 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	int i;
+	while (i < argc)
+	{
+		i++;
+	}
+	printf("%d\n", --1);
+	return (0);
+}
