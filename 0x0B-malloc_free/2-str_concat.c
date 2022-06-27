a03e0ccf6685fab08f152be5c2deea8deb9519e3
Babigdk <Babigdk@gmail.com>
2022-06-27 05:19:38 -0700

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,7 +15,7 @@ int _strlen(char *s)
 	return (a);
 }
 /**
- * _strcat - function to concatinate string with n byte
+ * _strncat - function to concatinate string with n byte
  * @dest: destionation
  * @src: source
  * @n: int of size
@@ -28,4 +28,36 @@ char *_strncat(char *dest, char *src, int n)
 	for (a = 0; a < n && src[a] != '\0'; a++)
 		dest[dest_len + a] = src[a];
 	return (dest);
+}
+/**
+ * *str_concat - return concatination
+ * @s1: pointing to distination
+ * @s2: to source
+ * Return: 0
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *ptr;
+	int size1, size2;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	size1 = _strlen(s1);
+	size2 = _strlen(s2);
+
+	ptr = malloc(((size1 + size2) + 1) * sizeof(char));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	_strncat(ptr, s1, size1);
+	_strncat(ptr, s2, size2);
+	ptr += '\0';
+	return (ptr);
 }
