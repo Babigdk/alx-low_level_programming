1db0d67e31bebf525dd18106fff2fec3fd153154
Babigdk <Babigdk@gmail.com>
2022-06-27 05:21:12 -0700

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,6 +10,7 @@
 int _strlen(char *s)
 {
 	int a;
+
 	for (a = 0; s[a] != '\0'; a++)
 		;
 	return (a);
@@ -24,6 +25,7 @@ int _strlen(char *s)
 char *_strncat(char *dest, char *src, int n)
 {
 	int dest_len, a;
+
 	dest_len = _strlen(dest);
 	for (a = 0; a < n && src[a] != '\0'; a++)
 		dest[dest_len + a] = src[a];
