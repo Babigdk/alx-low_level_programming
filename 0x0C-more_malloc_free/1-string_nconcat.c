3d0e9dc5d513da34c5db2160b9158676cd25e103
Babigdk <Babigdk@gmail.com>
2022-07-01 14:14:30 -0700

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,13 +13,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *a;
 
 	if (s1 == NULL)
-		s1 = "";
+		s1 = " ";
 
 	if (s2 == NULL)
-		s2 = "";
+		s2 = " ";
 	l1 = 0;
 	while (s1[l1])
 		l1++;
+
 	a = malloc(sizeof(*a) * l1 + n + 1);
 
 	if (a == NULL)
