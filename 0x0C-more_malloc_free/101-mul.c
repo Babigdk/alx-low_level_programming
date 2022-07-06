f16db0b47d837e82482b8c0e36db9725c900aaa2
Babigdk <Babigdk@gmail.com>
2022-07-06 06:44:45 -0700

    101 jj

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,10 +1,7 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <limits.h>
 /**
- * str_len - finds string len
+ * str_len - finds string length
  * @str: input pointer to string
  * Return: length of string
  */
