02a3674901a0a552918c60d5ceddacd052329572
Babigdk <Babigdk@gmail.com>
2022-07-06 06:32:39 -0700

    7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,18 +1,18 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
- * print_chessboard - prints 2D array of chessboard
- * @a: chessboard array
- * Return: void
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: Always 0 (Success)
  */
 void print_chessboard(char (*a)[8])
 {
-	int j = 1;
+	int i, n;
 
-	while (a[0][j - 1] != 0)
+	for (i = 0; i < 8; i++)
 	{
-		_putchar(a[0][j - 1]);
-		if (j > 1 && j % 8 == 0)
-			_putchar('\n');
-		j++;
+		for (n = 0; n < 8; n++)
+			_putchar(a[i][n]);
+		_putchar('\n');
 	}
 }
